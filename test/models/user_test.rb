c588c8915312d59f8980a89a15741c2a6ef6ce16
Kerollos Magdy <kerolloz@yahoo.com>
2020-08-23 23:25:30 +0200

    create user model

---
+++ test/models/user_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test 'user with a valid email should be valid' do
+    user = User.new(email: 'hamda@gmail.com', password_digest: 'my-password')
+    assert user.valid?
+  end
+
+  test 'user with invalid email should be invalid' do
+    user = User.new(email: 'hamada', password_digest: 'mayada')
+    assert_not user.valid?
+  end
+
+  test 'user with taken email should be invalid' do
+    other_user = users(:one)
+    user = User.new(email: other_user.email, password_digest: 'hamada')
+    assert_not user.valid?
+  end
+end
