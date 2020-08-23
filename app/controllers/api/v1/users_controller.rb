5895ee9081a082ab267a34b76934c682edefdd9e
Kerollos Magdy <kerolloz@yahoo.com>
2020-08-23 23:48:52 +0200

    add user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,23 @@
 class Api::V1::UsersController < ApplicationController
+  # GET /users/1
   def show
     render json: User.find(params[:id])
+  end
+
+  # POST /users/
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -13,4 +13,20 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_equal @user.email, json_response['email']
   end
 
+  test 'should create user' do
+    assert_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: "test@test.org", password: "test" } },
+      as: :json
+    end
+    assert_response :created
+  end
+
+  test 'should not create user with taken email' do
+    assert_no_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: @user.email, password: 'test' } },
+      as: :json
+    end
+    assert_response :unprocessable_entity
+  end
+
 end
