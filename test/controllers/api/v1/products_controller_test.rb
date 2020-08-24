620b8ccee1ab3660175e5cf3d77c570f318714e0
Kerollos Magdy <kerolloz@yahoo.com>
2020-08-24 17:43:09 +0200

    add product serializer to customize json response

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -10,7 +10,7 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     json_response = JSON.parse(self.response.body)
-    assert_equal @product.title, json_response['title']
+    assert_equal @product.title, json_response['data']['attributes']['title']
   end
 
   test 'should show products' do
