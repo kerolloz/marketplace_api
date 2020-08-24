bbd311b6c4fa819c11e461c1aed69505c2f82b09
Kerollos Magdy <kerolloz@yahoo.com>
2020-08-24 09:41:49 +0200

    add som validations to products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should have a positive price' do
+    product = products(:one)
+    product.price = -1
+    assert_not product.valid?
+  end
 end
