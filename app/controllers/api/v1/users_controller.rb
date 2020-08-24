f923e92c729c5ec73fd48503c760191e9a03a8a1
Kerollos Magdy <kerolloz@yahoo.com>
2020-08-24 09:26:47 +0200

    restrict actions for unauthorized users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
+  before_action :check_owner, only: %i[update destroy]
 
   # GET /users/1
   def show
@@ -40,5 +41,9 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def check_owner
+    head :forbidden unless @user.id == current_user&.id
   end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -30,22 +30,40 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update user' do
-    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } },
+    patch api_v1_user_url(@user), params: { user: { email: @user.email} },
+    headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
     as: :json
     assert_response :success
   end
 
+  test 'should forbid user update' do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email} },
+    as: :json
+    assert_response :forbidden
+  end
+
   test 'should not update user when invalid params are sent' do
     patch api_v1_user_url(@user), params: { user: { email: 'bad_email', password: '123456' } },
+    headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
     as: :json
     assert_response :unprocessable_entity
   end
 
   test 'should destroy user' do
     assert_difference('User.count', -1) do
-      delete api_v1_user_url(@user), as: :json
+      delete api_v1_user_url(@user),
+      headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
+      as: :json
     end
     assert_response :no_content
+  end
+
+  test 'should forbid user destroy' do
+    assert_no_difference('User.count') do
+      delete api_v1_user_url(@user),
+      as: :json
+    end
+    assert_response :forbidden
   end
 
 end
