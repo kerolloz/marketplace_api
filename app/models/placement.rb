2b1f72b501bacdea3484075a948776d1e32f8e80
Kerollos Magdy <kerolloz@yahoo.com>
2020-08-29 19:17:52 +0200

    decrease the product quantity by the placement quantity

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,4 +1,10 @@
 class Placement < ApplicationRecord
+  after_create :decrement_product_quantity!
+
   belongs_to :order, inverse_of: :placements
   belongs_to :product, inverse_of: :placements
+
+  def decrement_product_quantity!
+    product.decrement!(:quantity, quantity)
+  end
 end
--- test/models/placement_test.rb
+++ test/models/placement_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PlacementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @placement = placements(:one)
+  end
+
+  test 'should decrease the product quantity by the placemet quantity' do
+    product = @placement.product
+
+    assert_difference('product.quantity', -@placement.quantity) do
+      @placement.decrement_product_quantity!
+    end
+  end
 end
